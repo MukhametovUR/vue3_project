6f9c30a0da87a90674c45217e37481e2dff9c7b5
MukhametovUR <ural-lan4x4@yandex.ru>
2022-03-04 00:30:13 +0300

    Удаление постов и вывод сообщения Список пуст

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div>
+    <div v-if="posts.length > 0">
         <h3>Список пользователей</h3>
         <post-item 
         v-for="post in posts"
@@ -11,6 +11,9 @@
         >
     </post-item>
     </div>
+    <h2 v-else style="color:red">
+        Список пуст
+    </h2>
        
 
 </template>
