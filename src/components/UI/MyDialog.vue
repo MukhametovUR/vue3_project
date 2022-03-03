9d87d8170b475339824fa49f555f31c01512d9f6
MukhametovUR <ural-lan4x4@yandex.ru>
2022-03-04 00:48:50 +0300

    Добавлен props в диалоговое окно

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -1,10 +1,19 @@
 <template>
-    
+    <div class="dialog" v-if="show">
+        <div class="dialog__content"></div>
+
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'my-dialog',
+    props: {
+        show:{
+            type:Boolean,
+            default: false
+        }
+    }
 }
 </script>
 
