11fa25067019030b1ac8cc9c911ccd6da3d92d44
MukhametovUR <ural-lan4x4@yandex.ru>
2022-03-03 14:40:57 +0300

    Создание событий у дочернего комопнента и передавать в него параметры В родительском компоненте подписываемся на событие в дочернем

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
 
 <div class="app">
-    <form @submit.prevent>
-        <h4>Название поста</h4>
-        <input  v-bind:value="title"
-                @input="title = $event.target.value" 
-                type="text" 
-                class="input" 
-                placeholder="Название ">
-        <input v-bind:value="body" 
-                @input="body = $event.target.value" 
-                type="text" 
-                class="input" 
-                placeholder="Описание ">
-        <button class="btn" @click="createPost">Создать</button>
+    <post-form
+        @create="createPost"
+    >
+    </post-form>   
+    <post-list 
+        :posts="posts"
+    >
 
-    </form>
-    <div class="post" v-for="post in posts">
-        <div><strong>Название</strong> {{ post.title }}</div>
-        <div><strong>Описание</strong> {{ post.body }}</div>
-
-    </div>
+    </post-list>
 </div>
 
 </template>
 
 <script>
+import PostForm from '@/components/PostForm.vue';
+import PostList from '@/components/PostList.vue';
+
+
 export default {
+    components:{
+        PostList, PostForm
+
+    },
     data(){
         return {
             posts: [
@@ -35,20 +32,13 @@ export default {
             {id:3, title:'JavaScript3', body:'Описание поста3'},
             {id:4, title:'JavaScript3', body:'Описание поста3'},
             ],
-            title:'',
-            body:''
         }
     },
     methods: {
-        createPost(){
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body,
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
+        createPost(post, second, third){
+           console.log(post);
+            console.log(second);
+             console.log(third);
         },
  
 
@@ -65,29 +55,5 @@ export default {
 .app {
 padding: 20px;
 }
-form {
-    display: flex;
-    flex-direction: column;
-}
-.post {
-    padding: 15px;
-    margin-top: 10px;
-    border: 2px solid teal;
-}
 
-.input {
-    width: 100%;
-    border: 1px solid teal;
-    margin-top: 15px;
-    height: 40px;
-    padding-left: 10px;
-}
-.btn {
-    align-self: flex-end;
-    margin-top: 15px;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border-radius: 10px;
-}
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+       <div class="post" v-for="post in posts">
+        <div><strong>Название</strong> {{ post.title }}</div>
+        <div><strong>Описание</strong> {{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {                //Для двухстороннего связывания
+        posts: {            //То что ожидается на вход
+            type: Array,    //Указываем тип данных
+            requires: true, //Чем инициализируются
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    margin-top: 10px;
+    border: 2px solid teal;
+}
+</style>
